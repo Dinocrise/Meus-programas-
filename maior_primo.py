4afb58d955b8dd21f31fa4f0fd219dbdad1cacff
Dinocrise <sir.marcosdavi@gmail.com>
2020-07-09 21:50:43 -0300

    Genial 3 testes passaram

--- maior_primo.py
+++ maior_primo.py
@@ -1,12 +1,29 @@
-numero = int(input("Entre com o número: "))
-divisor = numero
-primo = 0
-while divisor != 1:
-    divisao = numero % divisor
-    if divisao == 0:
-        primo +=1
-    divisor -= 1
-if primo == 1:
-    print ("primo")
-else:
-    print("não primo")
+def eprimo(numero):
+    auxiliar = numero
+    resultado = False
+    if numero == 0:
+        return resultado
+    else:
+        primo = 0
+        while auxiliar != 1:
+            teste = numero % auxiliar
+            auxiliar -= 1
+            if teste == 0:
+                primo += 1
+        if primo == 1:
+            resultado = True
+        else:
+            resultado = False
+        return resultado
+
+def maior_primo(numero):
+    teste = eprimo(numero)
+    if teste:
+        return numero
+    else:
+        while not teste:
+            numero -= 1
+            teste = eprimo(numero)
+        return numero
+
+
---
+++ vogais.py
@@ -0,0 +1,6 @@
+def vogal(vogal):
+    result = False
+    if vogal == "a" or vogal == "A" or vogal == "e" or vogal == "E" or vogal == "i" or vogal == "I" or vogal ==  "o" or vogal == "O" or vogal == "u" or vogal == "U":
+        result = True
+    return result
+
